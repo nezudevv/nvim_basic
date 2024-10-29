2a44088e2833c87eef1de0f142144a0f626eaf48
Matt Breaux <mbreaux@freightwisellc.com>
2024-10-29 16:06:24 -0500

    added oil and new theme

--- init.lua
+++ init.lua
@@ -88,6 +88,19 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 -- Diagnostic keymaps
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
+vim.keymap.set("n", ";", ":")
+vim.keymap.set("n", ":", ";")
+
+-- Close the current buffer with <leader>x
+-- Move to the next buffer with Tab
+-- Move to the previous buffer with Shift-Tab
+vim.api.nvim_set_keymap("n", "<leader>x", ":bd<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<Tab>", ":bnext<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<S-Tab>", ":bprevious<CR>", { noremap = true, silent = true })
+
+-- oil
+vim.keymap.set("n", "-", "<CMD>:Oil --float<CR>", { desc = "Open current directory" })
+
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
@@ -755,13 +768,13 @@ require("lazy").setup({
 		-- change the command in the config to whatever the name of that colorscheme is.
 		--
 		-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-		"folke/tokyonight.nvim",
+		"sainnhe/sonokai",
 		priority = 1000, -- Make sure to load this before all the other start plugins.
 		init = function()
 			-- Load the colorscheme here.
 			-- Like many other themes, this one has different styles, and you could load
 			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-			vim.cmd.colorscheme("tokyonight-night")
+			vim.cmd.colorscheme("sonokai")
 
 			-- You can configure highlights by doing something like:
 			vim.cmd.hi("Comment gui=none")
@@ -857,6 +870,13 @@ require("lazy").setup({
 		keys = { -- load the plugin only when using it's keybinding:
 			{ "<leader>u", "<cmd>lua require('undotree').toggle()<cr>" },
 		},
+	},
+	{
+		"stevearc/oil.nvim",
+		opts = {},
+		-- Optional dependencies
+		dependencies = { { "echasnovski/mini.icons", opts = {} } },
+		-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
 	},
 	-- Adds git related signs to the gutter, as well as utilities for managing changes
 	-- NOTE: gitsigns is already included in init.lua but contains only the base
