fe1117317354bcf8e1fa9328b2aaf7ac17a6d6df
Nezudev <nezudevv@gmail.com>
2024-11-09 20:31:20 -0600

    commented transparent props

--- init.lua
+++ init.lua
@@ -771,9 +771,9 @@ require("lazy").setup({
 			vim.opt.background = "dark" -- set this to dark or light
 			vim.cmd("colorscheme oxocarbon")
 			-- -- transparent
-			vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-			vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+			-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+			-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+			-- vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
 		end,
 		-- Add in any other configuration;
 		--
