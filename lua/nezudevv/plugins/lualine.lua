8834b760d719705c2f69333f86f3d7e801e16e8d
Matt Breaux <mbreaux@freightwisellc.com>
2025-02-10 13:46:23 -0600

    cleanup

--- lua/nezudevv/plugins/lualine.lua
+++ lua/nezudevv/plugins/lualine.lua
@@ -14,14 +14,3 @@ return {
 		})
 	end,
 }
-
---
---
--- tabline = {
---   lualine_a = {},
---   lualine_b = {'branch'},
---   lualine_c = {'filename'},
---   lualine_x = {},
---   lualine_y = {},
---   lualine_z = {}
--- }
