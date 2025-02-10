1c99b039467402b6a8bab5f9020521025a7daf33
Matt Breaux <mbreaux@freightwisellc.com>
2025-02-10 13:56:16 -0600

    edited lualine elements

--- lua/nezudevv/plugins/lualine.lua
+++ lua/nezudevv/plugins/lualine.lua
@@ -3,6 +3,17 @@ return {
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
 		require("lualine").setup({
+			sections = {
+				lualine_a = {},
+				lualine_b = { "mode", "branch", "diff", "diagnostics" },
+				lualine_c = {},
+				lualine_x = { "encoding", "fileformat" },
+				lualine_y = { "location" },
+				lualine_z = {},
+				-- lualine_c = { "filename" },
+				-- lualine_x = { "encoding", "fileformat", "filetype" },
+				-- lualine_y = { "progress" },
+			},
 			tabline = {
 				lualine_a = { "buffers" },
 				lualine_b = {},
