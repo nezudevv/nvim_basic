4ebe92ce6fa2c07d7508836aa3d7279223eed71c
nezudevv <nezudevv@gmail.com>
2025-02-15 18:42:33 -0500

    linux 1st commit. removed noice and commented out llm

--- lua/nezudevv/plugins/llm.lua
+++ lua/nezudevv/plugins/llm.lua
@@ -1,19 +1,19 @@
 return {
-	"supermaven-inc/supermaven-nvim",
-	lazy = false,
-	config = function()
-		require("supermaven-nvim").setup({
-			keymaps = {
-				accept_suggestion = "<C-CR>",
-				clear_suggestion = "<C-]>",
-				accept_word = "<C-j>",
-			},
-			color = {
-				cterm = 244,
-			},
-			log_level = "info",
-			disable_inline_completion = false,
-			disable_keymaps = false,
-		})
-	end,
+	-- "supermaven-inc/supermaven-nvim",
+	-- lazy = false,
+	-- config = function()
+	-- 	require("supermaven-nvim").setup({
+	-- 		keymaps = {
+	-- 			accept_suggestion = "<C-CR>",
+	-- 			clear_suggestion = "<C-]>",
+	-- 			accept_word = "<C-j>",
+	-- 		},
+	-- 		color = {
+	-- 			cterm = 244,
+	-- 		},
+	-- 		log_level = "info",
+	-- 		disable_inline_completion = false,
+	-- 		disable_keymaps = false,
+	-- 	})
+	-- end,
 }
