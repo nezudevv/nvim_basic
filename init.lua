a1fc262f7c9786e5a6b2eae9abbfa8505bc964fd
Nezudev <nezudevv@gmail.com>
2025-06-14 11:15:38 -0500

    commented out problem code

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 --  NOTE: remaps and sets must happen before plugins are loaded (otherwise wrong leader will be used)
 require("nezudevv")
-vim.lsp.set_log_level("debug")
-vim.lsp.set_logging_file("/tmp/nvim_lsp_debug.log") -- Use this specific path
+-- vim.lsp.set_log_level("debug")
+-- vim.lsp.set_logging_file("/tmp/nvim_lsp_debug.log") -- Use this specific path
 
 -- Neovide Specific Config
 vim.g.neovide_cursor_vfx_mode = "pixiedust"
